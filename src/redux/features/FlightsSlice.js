b9db8470fd142bd5dffa44ab149ce0acd4b6392d
avahmetozdemir <avamertozdemir@gmail.com>
2023-02-17 12:26:47 +0300

    14- fetch today arrival flights method is added

--- src/redux/features/FlightsSlice.js
+++ src/redux/features/FlightsSlice.js
@@ -1,13 +1,46 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from "axios";
 
-const initialState = {};
+const initialState = {
+  flights: [],
+  flightsLoaded: false,
+};
+
+const config = {
+  headers: {
+    Accept: "application/json",
+    app_id: "47351075",
+    app_key: "ac353dccbad2d1bd8549dc774c6c04bc",
+    ResourceVersion: "v4",
+  },
+};
+
+// Fetch Default Today Arrival Flights
+export const fetchTodayArrival = createAsyncThunk(
+  "flights/todayArrival",
+  async () => {
+    const {
+      data: { flights },
+    } = await axios.get(
+      "http://localhost:5000/public-flights/flights?flightDirection=A&includedelays=false&page=1&sort=%2BscheduleTime",
+      config
+    );
+
+    return flights;
+  }
+);
 
 const FlightSlice = createSlice({
-  name: "Netflix",
+  name: "Flights",
   initialState,
-  extraReducers: (builder) => {},
+  extraReducers: (builder) => {
+    builder.addCase(fetchTodayArrival.fulfilled, (state, action) => {
+      state.flights = action.payload;
+      state.flightsLoaded = true;
+    });
+  },
 });
 
-export const { setGenres, setMovies } = FlightSlice.actions;
+export const {} = FlightSlice.actions;
 
 export default FlightSlice.reducer;
